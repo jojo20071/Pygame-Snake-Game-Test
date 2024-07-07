c0b695e9ee2f76c32ea4810ace4cc6eedd8f971d
Johannes <johanneshaederle18092@gmail.com>
2024-07-07 18:54:22 +0200

    added game logic

--- index.py
+++ index.py
@@ -23,3 +23,49 @@ score_font = pygame.font.SysFont(None, 35)
 
 pygame.mixer.init()
 # add the music later XD
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(screen, green, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    screen.blit(mesg, [screen_width / 6, screen_height / 3])
+
+def your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, white)
+    screen.blit(value, [0, 0])
+
+def main_menu():
+    menu = True
+    while menu:
+        screen.fill(black)
+        message("Press P to Play or Q to Quit", white)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    menu = False
+                    game_loop()
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+        
+def game_loop():
+    game_over = False
+    game_close = False
+
+    x1 = screen_width / 2
+    y1 = screen_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_list = []
+    length_of_snake = 1
+
+    foodx = round(random.randrange(0, screen_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, screen_height - snake_block) / 10.0) * 10.0
